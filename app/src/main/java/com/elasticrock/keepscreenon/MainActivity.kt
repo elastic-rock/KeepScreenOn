69051c0a1e70a8fd8f988bb0f61c1d4bc7653dc1
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-07-01 17:24:15 +0200

    Improve MainActivity

--- app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
@@ -6,7 +6,10 @@ import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,8 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.elasticrock.keepscreenon.ui.theme.KeepScreenOnTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,32 +33,33 @@ class MainActivity : ComponentActivity() {
             KeepScreenOnTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    KeepScreenOnApp(activity = this@MainActivity)
+                    KeepScreenOnAppCompact()
                 }
             }
         }
     }
-
-    fun isPermissionGranted(): Boolean {
-        return Settings.System.canWrite(applicationContext)
-    }
-
-    fun requestPermission() {
-        startActivity(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS))
-        recreate()
-    }
 }
 
 @Composable
-fun KeepScreenOnApp(modifier: Modifier = Modifier, activity: MainActivity) {
+fun KeepScreenOnAppCompact() {
+    val context = LocalContext.current
     Column(
-        modifier = modifier,
+        Modifier.padding(all = 8.dp)
+        .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.permission_description))
-        if (!activity.isPermissionGranted()) {
+        Text(text = stringResource(R.string.app_name),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(Modifier.height(8.dp))
+        Text(text = stringResource(R.string.permission_description),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(Modifier.height(8.dp))
+        if (!Settings.System.canWrite(context)) {
             Button(
-                onClick = { activity.requestPermission() }
+                onClick = { context.startActivity(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS))}
             ) {
                 Text(text = stringResource(R.string.grant_permission))
             }
@@ -60,13 +69,16 @@ fun KeepScreenOnApp(modifier: Modifier = Modifier, activity: MainActivity) {
                 Text(text = stringResource(R.string.permission_granted))
             }
         }
+        Spacer(Modifier.height(32.dp))
+        Text(text = stringResource(R.string.add_tile_instructions),
+            textAlign = TextAlign.Center)
     }
 }
 
 @Preview
 @Composable
-fun KeepScreenOnAppPreview() {
+fun KeepScreenOnAppCompactPreview() {
     KeepScreenOnTheme {
-        KeepScreenOnApp(activity = MainActivity())
+        KeepScreenOnAppCompact()
     }
 }
--- app/src/main/java/com/elasticrock/keepscreenon/ui/theme/Theme.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/ui/theme/Theme.kt
@@ -56,8 +56,8 @@ fun KeepScreenOnTheme(
     if (!view.isInEditMode) {
       SideEffect {
         val window = (view.context as Activity).window
-        window.statusBarColor = colorScheme.primary.toArgb()
-        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+          window.statusBarColor = colorScheme.background.toArgb()
+          WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
       }
     }
 
