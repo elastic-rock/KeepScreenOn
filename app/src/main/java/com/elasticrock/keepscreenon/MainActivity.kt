e4cafb06156cdfad1f4a65b56df53f865e8105c8
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-07-09 21:46:53 +0200

    Change UI, change versionName, and some more.

--- app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
@@ -9,28 +9,33 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.elasticrock.keepscreenon.ui.theme.KeepScreenOnTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -38,56 +43,81 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     KeepScreenOnApp()
+                    if (!Settings.System.canWrite(applicationContext)) {
+                        GrantPermissionDialog()
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KeepScreenOnApp() {
     val context = LocalContext.current
-    Column(
-        Modifier.padding(all = 8.dp).fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = stringResource(R.string.app_name),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
+    Scaffold(Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(R.string.app_name)) }
+            )
+        }
+    ) {padding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Button(onClick = {
+                    val statusBarService = context.getSystemService(StatusBarManager::class.java)
+                    statusBarService.requestAddTileService(
+                        ComponentName(context, QSTileService::class.java.name),
+                        context.getString(R.string.screen_timeout),
+                        Icon.createWithResource(context,R.drawable.outline_lock_clock_qs),
+                        {}) {}
+                }) {
+                    Text(text = stringResource(id = (R.string.add_qs_tile)))
+                }
+            } else {
+                Text(text = stringResource(R.string.add_tile_instructions),
+                    textAlign = TextAlign.Center)
+            }
+        }
+    }
+}
+
+@Composable
+fun GrantPermissionDialog() {
+    val context = LocalContext.current
+    val openDialog = remember { mutableStateOf(true) }
+
+    if (openDialog.value) {
+        AlertDialog(onDismissRequest = { openDialog.value = false },
+            title = { Text(text = stringResource(R.string.grant_permission)) },
+            dismissButton = {
+                TextButton(onClick = { openDialog.value = false }) {
+                    Text(text = stringResource(R.string.dismiss))
+                } },
+            text = { Text(text = stringResource(R.string.permission_description))},
+            confirmButton = {
+                TextButton(onClick = {
+                    context.startActivity(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS))
+                    openDialog.value = false }) {
+                    Text(text = stringResource(R.string.grant_permission))
+                }
+            }
         )
-        Spacer(Modifier.height(8.dp))
-        Text(text = stringResource(R.string.permission_description),
-            textAlign = TextAlign.Center,
-        )
-        Spacer(Modifier.height(8.dp))
-        if (!Settings.System.canWrite(context)) {
-            Button(
-                onClick = { context.startActivity(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS))}
-            ) {
-                Text(text = stringResource(R.string.grant_permission))
-            }
-        }
-        else {
-            Button(onClick = { }, enabled = false) {
-                Text(text = stringResource(R.string.permission_granted))
-            }
-        }
-        Spacer(Modifier.height(32.dp))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            Button(onClick = {
-                val statusBarService = context.getSystemService(StatusBarManager::class.java)
-                statusBarService.requestAddTileService(
-                    ComponentName(context, QSTileService::class.java.name),
-                    context.getString(R.string.screen_timeout),
-                    Icon.createWithResource(context,R.drawable.outline_lock_clock_qs),
-                    {}) {}
-            }) {
-                Text(text = stringResource(id = (R.string.add_qs_tile)))
-            }
-        } else {
-            Text(text = stringResource(R.string.add_tile_instructions),
-                textAlign = TextAlign.Center)
-        }
+    }
+}
+
+@Preview
+@Composable
+fun GrantPermissionDialogPreview() {
+    KeepScreenOnTheme {
+        GrantPermissionDialog()
     }
 }
 
