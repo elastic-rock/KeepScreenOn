59ab9426c0591566852b3c3e34de2eb917ba0830
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-09-03 19:30:45 +0200

    Better colour palette

--- app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.elasticrock.keepscreenon.ui.theme.KeepScreenOnTheme
 
@@ -144,4 +145,16 @@ fun GrantPermissionDialog() {
             }
         )
     }
+}
+
+@Preview
+@Composable
+fun KeepScreenOnAppPreview() {
+    KeepScreenOnApp()
+}
+
+@Preview
+@Composable
+fun GrantPermissionDialogPreview() {
+    GrantPermissionDialog()
 }
