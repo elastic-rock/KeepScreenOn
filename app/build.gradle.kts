7388e8f14e206652c1c021be7627790d1be726b6
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-11-16 22:07:02 +0100

    Bump up version + F-Droid changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elasticrock.keepscreenon"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.12.1"
+        versionCode = 17
+        versionName = "1.13.0"
         resourceConfigurations.addAll(listOf("cs", "en-rUS", "en-rGB","el-rGR", "fr-rFR"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
