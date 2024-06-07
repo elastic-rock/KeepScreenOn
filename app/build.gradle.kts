671a982050ade8d4b816280bb5bb161bd9c326a8
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-06-07 08:45:21 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
@@ -71,6 +71,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
     implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
-    implementation("androidx.lifecycle:lifecycle-service:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-service:2.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 }
