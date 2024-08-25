f92c0351fed7d01c3c1a25430a61a9dbd7968c65
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-08-25 13:11:46 +0200

    Migrate to version catalog and add Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
