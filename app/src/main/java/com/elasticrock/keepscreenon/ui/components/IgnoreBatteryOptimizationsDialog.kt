5e26cf7f9546cbe6d7ffc84c7f7bc48d6b02370d
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-09-07 12:35:15 +0200

    Add icon to IgnoreBatteryOptimizationsDialog

--- app/src/main/java/com/elasticrock/keepscreenon/ui/components/IgnoreBatteryOptimizationsDialog.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/ui/components/IgnoreBatteryOptimizationsDialog.kt
@@ -3,11 +3,15 @@ package com.elasticrock.keepscreenon.ui.components
 import android.content.Context
 import android.content.Intent
 import android.provider.Settings
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.EnergySavingsLeaf
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import com.elasticrock.keepscreenon.R
 
 @Composable
@@ -21,7 +25,10 @@ fun IgnoreBatteryOptimizationsDialog(
             context.startActivity(Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS))
             onDismissRequest.invoke()
         }) { Text(text = stringResource(id = R.string.confirm)) } },
-        title = { Text(text = stringResource(id = R.string.ignore_battery_optimizations)) },
-        text = { Text(text = stringResource(id = R.string.ignore_battery_optimizations_dialog_description)) }
+        title = { Text(text = stringResource(id = R.string.ignore_battery_optimizations), maxLines = 1, overflow = TextOverflow.Ellipsis) },
+        text = { Text(text = stringResource(id = R.string.ignore_battery_optimizations_dialog_description)) },
+        icon = {
+            Icon(imageVector = Icons.Filled.EnergySavingsLeaf, contentDescription = null)
+        }
     )
 }
