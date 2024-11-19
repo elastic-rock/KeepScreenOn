f8970831ed6dec14eae7c4fbb69653f65501d6e6
David Weis <hi@davidweis.eu>
2024-11-19 23:12:17 +0100

    Remove unused import directives

--- app/src/main/java/com/elasticrock/keepscreenon/ui/main/MainScreenViewModel.kt
+++ app/src/main/java/com/elasticrock/keepscreenon/ui/main/MainScreenViewModel.kt
@@ -4,13 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.elasticrock.keepscreenon.data.preferences.PreferencesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
