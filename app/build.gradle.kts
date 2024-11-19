302ef71c110ac480dc98a6e38d7bb9533dab61fc
David Weis <hi@davidweis.eu>
2024-11-19 23:08:21 +0100

    Bump-up version + F-Droid changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.elasticrock.keepscreenon"
         minSdk = 24
         targetSdk = 35
-        versionCode = 34
-        versionName = "1.20.3"
+        versionCode = 35
+        versionName = "1.20.4"
         resourceConfigurations.addAll(listOf("cs", "en-rUS", "en-rGB","el-rGR", "fr-rFR", "zh-rCN", "ar", "ja"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
