4a2a391be7c3834bed9fc338c28c38e1679d8dc9
David Weis <hi@davidweis.eu>
2025-08-12 21:14:24 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -43,9 +45,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    kotlin.compilerOptions.jvmTarget = JvmTarget.JVM_11
     buildFeatures {
         compose = true
         buildConfig = true
